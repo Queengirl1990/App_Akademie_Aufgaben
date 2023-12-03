92982bef947dcd25e966256ee8f2b52a96bbdc91
Anja Brühl <126663193+Queengirl1990@users.noreply.github.com>
2023-12-03 20:02:09 +0100

    Create error-handling.dart

---
+++ error-handling.dart
@@ -0,0 +1,11 @@
+void main() {
+  int result;
+  for (int i = 10; i >= 0; i--) {
+    try {
+      result = 10 ~/ i;
+      print(result);
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+}
