5d740a3381c8d870b26f0dfd0767ac0edbf334f1
Anja Brühl <126663193+Queengirl1990@users.noreply.github.com>
2023-12-03 20:08:16 +0100

    Create styles.dart

---
+++ styles.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+const Color darkRed = Color(0xFFAC5859);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: darkRed,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle appBarTextStyle = const TextStyle(
+      fontFamily: 'DancingScript',
+      fontWeight: FontWeight.bold,
+      fontSize: 24,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Mein Profil",
+          style: appBarTextStyle,
+        ),
+        backgroundColor: darkRed,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: Container(
+          color: darkRed,
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  MyCircularAvatar(),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.grey,
+                      Colors.white
+                    ], // Farbverlauf von grau nach weiß
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 0),
+                    ),
+                  ],
+                ),
+                padding: const EdgeInsets.all(20),
+                child: const Column(
+                  children: [
+                    Text(
+                      "Überschrift in Bold",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Unterstrichene Überschrift",
+                      style: TextStyle(
+                        decoration: TextDecoration.underline,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyCircularAvatar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.topRight,
+      margin: const EdgeInsets.only(top: 20, right: 20),
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 0),
+          ),
+        ],
+      ),
+      child: ClipOval(
+        child: Image.asset(
+          'assets/images/avatar.png',
+          width: 50,
+          height: 50,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
