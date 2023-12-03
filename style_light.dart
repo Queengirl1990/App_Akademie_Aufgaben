af20700cc2c52aa96c1c654e309ca01db24bfbd9
Anja Brühl <126663193+Queengirl1990@users.noreply.github.com>
2023-12-03 20:12:14 +0100

    Create style_light.dart

---
+++ style_light.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+final TextStyle boldTextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+final TextStyle regularTextStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 20,
+);
