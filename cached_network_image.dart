51663a89a96ce10cd87b712c12da6f74a8fbe59c
Anja Brühl <126663193+Queengirl1990@users.noreply.github.com>
2023-12-03 19:59:44 +0100

    Create cached_network_image.dart

---
+++ cached_network_image.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Cached Network Image Example'),
+        ),
+        body: Center(
+          child: CachedNetworkImage(
+            imageUrl:
+                'https://www.drachenmond.de/cdn/shop/files/drachenmond-book-elements-farbschnittansicht_02bac1f7-f430-45e0-8eb5-fabf49dc00cb.png?v=1696642296',
+            placeholder: (context, url) => const CircularProgressIndicator(),
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          ),
+        ),
+      ),
+    );
+  }
+}
