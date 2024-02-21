9f66788514e614a33511825838cd23e495d077e4
Anja Brühl <126663193+Queengirl1990@users.noreply.github.com>
2024-02-21 12:41:58 +0100

    Create aufgabe2.dart

---
+++ aufgabe2.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Switch Example",
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 238, 238, 238),
+        appBar: AppBar(
+          title: const Text('App Akademie'),
+        ),
+        body: const MyContent(),
+      ),
+    );
+  }
+}
+
+class MyContent extends StatefulWidget {
+  const MyContent({super.key});
+
+  @override
+  _MyContentState createState() => _MyContentState();
+}
+
+class _MyContentState extends State<MyContent> {
+  //switch prüfen
+  bool isSwitchOn = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Center(
+          child: Text(
+            "Hello World!",
+            style: TextStyle(fontSize: 18, color: Colors.black),
+          ),
+        ),
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //erstellen zwei farblich Unterschiedlicher Container
+            Container(
+              color: Colors.red,
+              child: const SizedBox(
+                width: 120,
+                height: 120,
+              ),
+            ),
+            const SizedBox(width: 40),
+            Container(
+              color: Colors.green,
+              child: const SizedBox(
+                width: 120,
+                height: 120,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 150),
+        Center(
+          child: Switch(
+            value: isSwitchOn,
+            onChanged: (value) {
+              //interaktion mit dem switch
+              setState(() {
+                isSwitchOn = value;
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
